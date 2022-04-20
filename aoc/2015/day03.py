7c1c0920c41912cb9970c7bca750a29ee143d1e3
rpmartz <ryan@ryanpmartz.com>
2022-04-20 06:39:56 -0700

    2015 day 3

--- aoc/2015/day03.py
+++ aoc/2015/day03.py
@@ -3,31 +3,37 @@ from collections import namedtuple
 HouseCoordinate = namedtuple('HouseCoordinate', ['x', 'y'])
 
 
+def move(position: HouseCoordinate, instruction: str) -> HouseCoordinate:
+    if instruction == '>':
+        new_position = HouseCoordinate(position.x + 1, position.y)
+    elif instruction == '^':
+        new_position = HouseCoordinate(position.x, position.y + 1)
+    elif instruction == '<':
+        new_position = HouseCoordinate(position.x - 1, position.y)
+    elif instruction == 'v':
+        new_position = HouseCoordinate(position.x, position.y - 1)
+    else:
+        raise RuntimeError(f'Unexpected instruction {instruction}')
+
+    return new_position
+
+
 if __name__ == '__main__':
     with open('data/day03.txt', 'r') as infile:
         chars = infile.read()
 
     visited: set[HouseCoordinate] = set()
 
-    current = HouseCoordinate(0, 0)
-    visited.add(current)
+    santa_position = HouseCoordinate(0, 0)
+    robot_santa_position = HouseCoordinate(0, 0)
+    visited.add(santa_position)
 
-    for instruction in chars:
-        if instruction == '>':
-            current = HouseCoordinate(current.x + 1, current.y)
-        elif instruction == '^':
-            current = HouseCoordinate(current.x, current.y + 1)
-        elif instruction == '<':
-            current = HouseCoordinate(current.x - 1, current.y)
-        elif instruction == 'v':
-            current = HouseCoordinate(current.x, current.y - 1)
+    for index, instruction in enumerate(chars):
+        if index % 2 == 0:
+            santa_position = move(santa_position, instruction)
+            visited.add(santa_position)
         else:
-            raise RuntimeError(f'Unexpected instruction {instruction}')
-
-        visited.add(current)
+            robot_santa_position = move(robot_santa_position, instruction)
+            visited.add(robot_santa_position)
 
     print(len(visited))
-
-
-
-
