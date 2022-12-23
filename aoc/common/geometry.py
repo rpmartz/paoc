a60067ce2cff29e0783fc8bc3f14eb089784551f
rpmartz <ryan@ryanpmartz.com>
2022-12-23 07:28:14 -0800

    move Point to geometry package and add Manhattan distance function

--- aoc/common/geometry.py
+++ aoc/common/geometry.py
@@ -1,3 +1,8 @@
 from collections import namedtuple
 
 Point3D = namedtuple('Point3D', 'x y z')
+Point = namedtuple('Point', 'x, y')
+
+
+def manhattan_distance(p1, p2):
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
