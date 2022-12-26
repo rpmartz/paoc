3e23c540c6789a32b6cc09c261b68204600a0fdb
rmartz <ryan@fundrise.com>
2022-12-26 09:25:58 -0800

    add more utils functions

--- clojure/aoc/src/aoc/utils.clj
+++ clojure/aoc/src/aoc/utils.clj
@@ -2,16 +2,25 @@
   (:require [clojure.string :as str]))
 
 (defn read-lines [filename]
-  (str/split-lines (slurp (str "../../inputs/" filename)))
-  )
+  (str/split-lines (slurp (str "../../inputs/" filename))))
 
 (defn read-input [filename]
-  (slurp (str "../../inputs/" filename))
-  )
+  (slurp (str "../../inputs/" filename)))
 
 (defn ints [s]
   (re-seq #"-?[0-9]+" s))
 
+(defn coordinate-square
+  "Returns a sequence of all points in the 2D square made by the points"
+  [start-x start-y end-x end-y]
+  (for [x (range start-x end-x)
+        y (range start-y end-y)]
+    [x y]))
+
+(defn grid [numrows numcols]
+  (for [x (range numrows)
+        y (range numcols)
+        ] [x y]))
 
 
 
