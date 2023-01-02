03aa1396f5b41867bd3756b174234ce327861643
rpmartz <ryan@ryanpmartz.com>
2023-01-02 09:45:29 -0800

    add assertion to catch bad args

--- clojure/aoc/test/aoc/y21/day18_test.clj
+++ clojure/aoc/test/aoc/y21/day18_test.clj
@@ -6,4 +6,9 @@
   (testing "Test splitting examples"
     (is (= [5 5] (split 10)))
     (is (= [5 6] (split 11)))
-    (is (= [6 6] (split 12)))))
+    (is (= [6 6] (split 12))))
+  
+  (testing "Throws exception if given list"
+    (is (thrown? java.lang.AssertionError (split [4 4])))
+    )
+  )
