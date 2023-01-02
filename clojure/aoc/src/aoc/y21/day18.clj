f42c534e9287f86c998ff527958016ddc69ad259
rpmartz <ryan@ryanpmartz.com>
2023-01-02 10:29:31 -0800

    add simple addition example

--- clojure/aoc/src/aoc/y21/day18.clj
+++ clojure/aoc/src/aoc/y21/day18.clj
@@ -7,6 +7,10 @@
 
 (defn explode [num])
 
+(defn sfish-add [l r]
+  [l r]
+  )
+
 (defn split
   "To split a regular number, replace it with a pair; the left element of the pair should be the regular number divided by two and rounded down, while the right element of the pair should be the regular number divided by two and rounded up. For example, 10 becomes [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on."
   [num]
