be133608284b448e6028c539324228a252981705
rmartz <martzrp@gmail.com>
2023-01-06 20:56:07 -0800

    refactor parsing to points

--- python/aoc/y22/day15.py
+++ python/aoc/y22/day15.py
@@ -1,5 +1,12 @@
 from python.aoc.common.geometry import Point, manhattan_distance
 from python.aoc.common.parsing import ints
+from dataclasses import dataclass
+
+@dataclass
+class Pair:
+    sensor: Point
+    beacon: Point
+    range: int
 
 
 def get_input():
@@ -17,7 +24,9 @@ def parse_lines(lines):
         sensor = Point(coordinates[0], coordinates[1])
         beacon = Point(coordinates[2], coordinates[3])
 
-        pairs.append((sensor, beacon))
+        coverage_range = sensor.manhattan_distance_to(beacon)
+
+        pairs.append(Pair(sensor, beacon, coverage_range))
 
     return pairs
 
@@ -26,47 +35,14 @@ def part_one():
     lines = get_input()
     pairs = parse_lines(lines)
 
-    covered_locations = set()
-    min_x = 0
-    max_x = -10000000
 
-    radii = {}
-
-    for pair in pairs:
-        sensor = pair[0]
-        beacon = pair[1]
-
-        distance_to_closest_beacon = manhattan_distance(sensor, beacon)
-        radii[sensor] = distance_to_closest_beacon
-
-        min_x = min([min_x, sensor.x - distance_to_closest_beacon])
-        max_x = max([max_x, sensor.x + distance_to_closest_beacon])
-
-    num_checked = 0
-    for x in range(min_x, max_x + 1):
-
-        pt = Point(x, 2000000)
-        num_checked += 1
-
-        for pair in pairs:
-            sensor = pair[0]
-            dist_to_sensor = manhattan_distance(sensor, pt)
-
-            if dist_to_sensor <= radii[sensor]:
-                covered_locations.add(pt)
-                break
-
-    print(f'x range: {min_x} to {max_x}')
-    # need min x and max x to determine length of row
-    print(f'num covered locations: {len(covered_locations)}')
-    print(f'num checked: {num_checked}')
-    print(f'answer: {num_checked - len(covered_locations)}')
-    # 334399 too low
-    # 334400 too low
-    # 334402 too low
-    # 1642660 not correct
-    # 1642657 not correct
 
 
 if __name__ == '__main__':
     part_one()
+
+ # 334399 too low
+# 334400 too low
+# 334402 too low
+# 1642660 not correct
+# 1642657 not correct
