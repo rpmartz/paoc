027c55fe935e50d18068aca69b72e97b113cc121
rpmartz <ryan@ryanpmartz.com>
2023-02-01 17:28:45 -0800

    test input for 2022.15

--- python/aoc/y22/day15.py
+++ python/aoc/y22/day15.py
@@ -1,12 +1,5 @@
 from python.aoc.common.geometry import Point, manhattan_distance
 from python.aoc.common.parsing import ints
-from dataclasses import dataclass
-
-@dataclass
-class Pair:
-    sensor: Point
-    beacon: Point
-    range: int
 
 
 class Pair:
@@ -26,6 +19,23 @@ def get_input():
     return lines
 
 
+def get_test_input():
+    return ['Sensor at x=2, y=18: closest beacon is at x=-2, y=15',
+            'Sensor at x=9, y=16: closest beacon is at x=10, y=16',
+            'Sensor at x=13, y=2: closest beacon is at x=15, y=3',
+            'Sensor at x=12, y=14: closest beacon is at x=10, y=16',
+            'Sensor at x=10, y=20: closest beacon is at x=10, y=16',
+            'Sensor at x=14, y=17: closest beacon is at x=10, y=16',
+            'Sensor at x=8, y=7: closest beacon is at x=2, y=10',
+            'Sensor at x=2, y=0: closest beacon is at x=2, y=10',
+            'Sensor at x=0, y=11: closest beacon is at x=2, y=10',
+            'Sensor at x=20, y=14: closest beacon is at x=25, y=17',
+            'Sensor at x=17, y=20: closest beacon is at x=21, y=22',
+            'Sensor at x=16, y=7: closest beacon is at x=15, y=3',
+            'Sensor at x=14, y=3: closest beacon is at x=15, y=3',
+            'Sensor at x=20, y=1: closest beacon is at x=15, y=3']
+
+
 def parse_lines(lines):
     pairs = []
     for line in lines:
@@ -36,33 +46,44 @@ def parse_lines(lines):
 
         coverage_range = sensor.manhattan_distance_to(beacon)
 
-        pairs.append(Pair(sensor, beacon, coverage_range))
+        pairs.append(Pair(sensor, beacon))
 
     return pairs
 
 
 def part_one():
-    lines = get_input()
+    lines = get_test_input()
     pairs = parse_lines(lines)
 
     pairs_in_range = set()
+    Y = 2_000_000
 
+    ranges = set()
     for pair in pairs:
-        distance_to_line = pair.sensor.manhattan_distance_to(Point(pair.sensor.x, 200000))
-
-        if distance_to_line <= pair.range:
-            pairs_in_range.add(pair)
-            print(f'Sensor at {pair.sensor} is able to reach line')
+        d = pair.distance()
+        dy = abs(pair.sensor.y - Y)
+        if dy > d:  # sensor cannot reach line we care about
+            ranges.add(range(-1, -1))
         else:
-            print(f'Sensor at {pair.sensor} NOT in range of line')
+            dx = d - dy  # "remaining range" of horizontal coverage on line
+            ranges.add(range(pair.sensor.x - dx, pair.sensor.x + dx + 1))
+            pairs_in_range.add(pair)
 
+    range_union = set().union(*ranges)
+    # x coordinates of any beacons on the y line
+    y_beacon_locations = {pair.sensor.x for pair in pairs_in_range if pair.beacon.y == Y}
 
+    answer = len(range_union) - len(y_beacon_locations)
+    print(answer)
+
+    # 4717629 - wrong
+    # 4717628 - wrong
 
 
 if __name__ == '__main__':
     part_one()
 
- # 334399 too low
+# 334399 too low
 # 334400 too low
 # 334402 too low
 # 1642660 not correct
