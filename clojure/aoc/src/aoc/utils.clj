d78610bd9e326d67221896cf789991b421aa9fc1
rmartz <martzrp@gmail.com>
2023-03-07 21:09:49 -0800

    remove duplicate ints function

--- clojure/aoc/src/aoc/utils.clj
+++ clojure/aoc/src/aoc/utils.clj
@@ -7,9 +7,6 @@
 (defn read-input [filename]
   (slurp (str "../../inputs/" filename)))
 
-(defn ints [s]
-  (re-seq #"-?[0-9]+" s))
-
 (defn coordinate-square
   "Returns a sequence of all points in the 2D square made by the points"
   [start-x start-y end-x end-y]
