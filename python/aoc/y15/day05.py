8186a05185d48a6226660718694406875cc3385a
rmartz <martzrp@gmail.com>
2023-06-22 08:17:15 -0700

    15.2 golfed

--- python/aoc/y15/day05.py
+++ python/aoc/y15/day05.py
@@ -1,34 +1,7 @@
-import os
-
 with open('data/day05.txt') as f:
     lines = f.read().splitlines()
 
-is_nice = lambda s: any(a == b for a, b in zip(s, s[2:]))
+is_nice = lambda it: (any(it[i:i + 2] in it[:i] for i in range(len(it) - 1)) and
+                      any(a == b for a, b in zip(it, it[2:])))
 
-def has_repeating_pair(line):
-    for i in range(len(line) - 1):
-        if line[i:i+2] in line[:i]:
-            return True
-
-def has_valid_triple(line):
-    for a, b in zip(line, line[2:]):
-        if a == b:
-            return True
-
-
-count = 0
-
-for line in lines:
-    has_pair = False
-    valid_triple = False
-
-    if has_repeating_pair(line):
-        has_pair = True
-    if has_valid_triple(line):
-        valid_triple = True
-
-    if has_pair and valid_triple:
-        count += 1
-
-# print(sum(map(is_nice, lines)))
-print(count)
+print(sum(map(is_nice, lines)))
