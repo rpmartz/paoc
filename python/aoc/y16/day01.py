aadabc2869aac8e2b7262935a5ded1f6160e7eee
rmartz <martzrp@gmail.com>
2023-06-25 11:18:32 -0700

    part 1

--- python/aoc/y16/day01.py
+++ python/aoc/y16/day01.py
@@ -1,5 +1,5 @@
 with open('data/day01.txt', 'r') as f:
-    directions = f.read()
+    directions = [d.strip() for d in f.read().split(',')]
 
 cardinal_directions = {'N', 'S', 'E', 'W'}
 turn_directions = {'L', 'R'}
@@ -17,3 +17,26 @@ def new_heading(current_heading, instruction_direction):
 
     return heading_map[current_heading][instruction_direction]
 
+def parse_dir(direction):
+    turn_dir = direction[0]
+    magnitude = int(direction[1:])
+
+    return turn_dir, magnitude
+
+current_heading = 'N'
+location = (0, 0)
+
+for direction in directions:
+    turn_dir, distance = parse_dir(direction)
+    current_heading = new_heading(current_heading, turn_dir)
+    if current_heading == 'N':
+        location = (location[0], location[1] + distance)
+    elif current_heading == 'S':
+        location = (location[0], location[1] - distance)
+    elif current_heading == 'E':
+        location = (location[0] + distance, location[1])
+    elif current_heading == 'W':
+        location = (location[0] - distance, location[1])
+
+print(f'Location is {location}')
+print(f'Manhattan distance is {abs(location[0] - 0) + abs(location[1] - 0)}')
