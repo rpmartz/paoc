cecc2570fc80f49faaac0d9f3857f6d8718711b1
rmartz <martzrp@gmail.com>
2023-07-27 07:21:57 -0700

    format with black

--- python/aoc/y21/day_24_parser.py
+++ python/aoc/y21/day_24_parser.py
@@ -1,32 +1,40 @@
-
-
-with open('data/day24.txt', 'r') as f:
+with open("data/day24.txt", "r") as f:
     lines = [l.strip() for l in f.readlines()]
 
 results = []
 for line in lines:
-    if line == 'inp w':
+    if line == "inp w":
         results.append(line)
         continue
 
-    components = line.split(' ')
+    components = line.split(" ")
     instruction = components[0]
     first = components[1]
     second = components[2]
 
     operand = None
-    if instruction == 'mul':
-        results.append(f'{first.upper()} = {first.upper()} * {second.upper()}; // {line}')
-    elif instruction == 'add':
-        results.append(f'{first.upper()} = {first.upper()} + {second.upper()}; // {line}')
-    elif instruction == 'div':
-        results.append(f'{first.upper()} = {first.upper()} / {second.upper()}; // {line}')
-    elif instruction == 'mod':
-        results.append(f'{first.upper()} = {first.upper()} % {second.upper()}; // {line}')
-    elif instruction == 'eql':
-        results.append(f'{first.upper()} = {first.upper()} == {second.upper()} ? 1 : 0; // {line}')
+    if instruction == "mul":
+        results.append(
+            f"{first.upper()} = {first.upper()} * {second.upper()}; // {line}"
+        )
+    elif instruction == "add":
+        results.append(
+            f"{first.upper()} = {first.upper()} + {second.upper()}; // {line}"
+        )
+    elif instruction == "div":
+        results.append(
+            f"{first.upper()} = {first.upper()} / {second.upper()}; // {line}"
+        )
+    elif instruction == "mod":
+        results.append(
+            f"{first.upper()} = {first.upper()} % {second.upper()}; // {line}"
+        )
+    elif instruction == "eql":
+        results.append(
+            f"{first.upper()} = {first.upper()} == {second.upper()} ? 1 : 0; // {line}"
+        )
     else:
-        raise Exception(f'Should never get here. Line [{line}]')
+        raise Exception(f"Should never get here. Line [{line}]")
 
 for r in results:
     print(r)
