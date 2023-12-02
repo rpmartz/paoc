11de83f344d305ff770284e984f456eec5c34514
rmartz <martzrp@gmail.com>
2023-12-02 05:48:41 -0800

    2023.2.1 solution

---
+++ python/aoc/y23/day02.py
@@ -0,0 +1,47 @@
+
+
+with open('data/day02.txt', 'r') as ifile:
+    lines = [l.strip() for l in ifile.readlines()]
+
+
+games = {}
+
+for line in lines:
+    components = line.split(':')
+    game_no = components[0].split(' ')[-1]
+
+    roll_data = components[1]
+
+    handfulls = roll_data.split(';')
+
+    reds = []
+    blues = []
+    greens = []
+
+    for handfull in handfulls:
+        combos = handfull.split(',')
+
+        for quantity in combos:
+            q_stripped = quantity.strip()
+            if 'red' in q_stripped:
+                reds.append(int(q_stripped.replace(' red', '')))
+            elif 'blue' in q_stripped:
+                blues.append(int(q_stripped.replace(' blue', '')))
+            elif 'green' in q_stripped:
+                greens.append(int(q_stripped.replace(' green', '')))
+            else:
+                raise ValueError(f'Unexpected value {quantity}')
+
+    games[int(game_no)] = {
+        'red': max(reds),
+        'blue': max(blues),
+        'green': max(greens)
+    }
+
+valid_game_numbers = set()
+
+for game, maxes in games.items():
+    if maxes['red'] <= 12 and maxes['blue'] <= 14 and maxes['green'] <= 13:
+        valid_game_numbers.add(game)
+
+print(sum(valid_game_numbers))
