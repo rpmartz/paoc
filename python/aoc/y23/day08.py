880ad30683a949a6c0ae0f2979bdd2fa2b2f39d8
rmartz <martzrp@gmail.com>
2023-12-16 19:32:34 -0800

    refactor

--- python/aoc/y23/day08.py
+++ python/aoc/y23/day08.py
@@ -1,44 +1,46 @@
 from itertools import cycle
 import re
 
+
 def read_lines():
-    with open('data/day08.txt') as f:
+    with open("data/day08.txt") as f:
         lines = [l.strip() for l in f.readlines()]
 
     return lines
 
 
+def part_one(node_mappings, instructions):
+    current_node = "AAA"
+    steps = 0
 
-lines = read_lines()
-instructions = lines[0]
-mappings = lines[2:]
+    for instr in cycle(instructions):
+        node = node_mappings[current_node]
+        steps += 1
 
-nodes = {}
-for line in mappings:
-    node, left, right = re.findall(r'\w+', line)
-    nodes[node] = (left, right)
+        if instr == "L":
+            current_node = node[0]
+        elif instr == "R":
+            current_node = node[1]
+        else:
+            raise ValueError(f"Unknown instruction: {instr}")
 
-print(nodes)
+        if current_node == "ZZZ":
+            break
 
-current_node = 'AAA'
-steps = 0
-
-for instr in cycle(instructions):
-    node = nodes[current_node]
-    steps += 1
-
-    if instr == 'L':
-        current_node = node[0]
-    elif instr == 'R':
-        current_node = node[1]
-    else:
-        raise ValueError(f'Unknown instruction: {instr}')
-
-    if current_node == 'ZZZ':
-        break
-
-print(steps)
+    print(steps)
 
 
+if __name__ == '__main__':
+
+    lines = read_lines()
+    instructions = lines[0]
+    mappings = lines[2:]
+
+    nodes = {}
+    for line in mappings:
+        node, left, right = re.findall(r"\w+", line)
+        nodes[node] = (left, right)
+
+    part_one(nodes, instructions)
 
 
