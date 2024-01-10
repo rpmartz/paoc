b19fc3868f18831803d5e5289ae0fe1b4d21e689
rmartz <ryan@fundrise.com>
2024-01-10 19:23:48 +0200

    format

--- python/aoc/y15/day07.py
+++ python/aoc/y15/day07.py
@@ -6,11 +6,14 @@ operators = {
     'LSHIFT': operator.lshift,
     'RSHIFT': operator.rshift,
 }
+
+
 def read_lines():
     with open('data/day07.txt', 'r') as f:
         lines = [l.strip() for l in f.readlines()]
 
     return lines
+
 
 def symbol_table(lines):
     table = {}
@@ -19,6 +22,7 @@ def symbol_table(lines):
         table[rhs] = lhs
 
     return table
+
 
 def determine_signal(wire_name, table):
     if wire_name.isnumeric():
@@ -36,8 +40,8 @@ def determine_signal(wire_name, table):
         operation = operators[op]
         return operation(determine_signal(lhs, table), determine_signal(rhs, table))
 
+
 if __name__ == '__main__':
     lines = read_lines()
     stable = symbol_table(lines)
     print(determine_signal('a', stable))
-
