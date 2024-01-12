16717873010922d64e53a425ad16b84bad12bdd9
rmartz <ryan@fundrise.com>
2024-01-12 20:33:32 +0200

    2015 day08 part2

--- python/aoc/y15/day08.py
+++ python/aoc/y15/day08.py
@@ -18,10 +18,7 @@ def determine_mem_chars(line):
             cursor += 1
         elif char == '\\':
             next_char = line[cursor + 1]
-            if next_char == '"':
-                cursor += 2
-                mem_chars += 1
-            elif next_char == '\\':
+            if next_char in {'"', '\\'}:
                 cursor += 2
                 mem_chars += 1
             elif next_char == 'x':
@@ -37,14 +34,28 @@ def determine_mem_chars(line):
     return mem_chars
 
 
+def encode_line(line):
+    cursor = 0
+    encoded_chars = ['"']
+    while cursor < len(line):
+        char = line[cursor]
+        if char in {'"', '\\'}:
+            encoded_chars.append("\\")
+
+        encoded_chars.append(char)
+        cursor += 1
+
+    encoded_chars.append('"')
+    return ''.join(encoded_chars)
+
 if __name__ == '__main__':
     lines = read_lines()
 
-    code_chars = 0
-    mem_chars = 0
+    original_chars = 0
+    encoded_chars = 0
     for line in lines:
-        code_chars += len(line)
-        mem_chars += determine_mem_chars(line)
+        original_chars += len(line)
+        encoded_chars += len(encode_line(line))
 
-    difference = code_chars - mem_chars
+    difference = encoded_chars - original_chars
     print(difference)
