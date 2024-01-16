4cec7b1fbd68fcd2111cfad3dd0fa65cfddb09ae
rmartz <martzrp@gmail.com>
2024-01-16 06:01:14 -0800

    correct implementation of wrong algorithm

--- python/aoc/y15/day09.py
+++ python/aoc/y15/day09.py
@@ -1,7 +1,8 @@
 import re
 from collections import defaultdict
 import pprint
-# input is a bidirectional weighted graph
+
+# input is an undirected weighted graph
 def load_graph():
     distances = defaultdict(dict)
 
@@ -13,11 +14,46 @@ def load_graph():
         src, dest, dist = result.groups()
 
         distances[src][dest] = int(dist)
-
+        distances[dest][src] = int(dist)
 
     return distances
 
-if __name__ == '__main__':
+
+def load_edges():
+    with open("data/day09.txt", "r") as ifile:
+        lines = [l.strip() for l in ifile.readlines()]
+
+    edges = []
+
+    for line in lines:
+        result = re.match("(\w+) to (\w+) = (\d+)", line)
+        src, dest, dist = result.groups()
+
+        edges.append((int(dist), src, dest))
+
+    return edges
+
+
+if __name__ == "__main__":
     dists = load_graph()
+    edges = sorted(load_edges())
 
+    visited = set()
+    distance_traveled = 0
 
+    # start randomly at Faerun
+    visited.add("Faerun")
+
+    while len(visited) < len(dists.keys()) - 1:
+        for dist, src, dest in edges:
+            if src not in visited and dest in visited:
+                print(f"\tAdding edge {src} - {dest} of cost {dist}")
+                visited.add(src)
+                distance_traveled += dist
+            elif src in visited and dest not in visited:
+                print(f"\tAdding edge {src} - {dest} of cost {dist}")
+                visited.add(dest)
+                distance_traveled += dist
+
+    print(sorted(visited))
+    print(distance_traveled)
