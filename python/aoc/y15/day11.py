6c77a9252d1239d47820e5ea38dc938cc4ad7b27
rmartz <martzrp@gmail.com>
2024-01-27 17:06:38 -0800

    day 11 wip

--- python/aoc/y15/day11.py
+++ python/aoc/y15/day11.py
@@ -16,9 +16,12 @@ index_to_digits = dict(zip(range(len(abet)), abet))
 def meets_requirement(pw) -> bool:
     has_ascending_run = any([triplet in pw for triplet in triplets])
     no_banned_chars = all([c not in pw for c in banned_chars])
-    has_two_pairs = any([a in pw and b in pw for a, b in product(letter_pairs,  repeat=2)])
+    has_two_pairs = any(
+        [a != b and a in pw and b in pw for a, b in product(letter_pairs, repeat=2)]
+    )
 
     return all([has_ascending_run, no_banned_chars, has_two_pairs])
+
 
 def increment_password(pw):
     orig = pw
@@ -26,27 +29,28 @@ def increment_password(pw):
     while index > -1:
         if pw != orig and meets_requirement(pw):
             return pw
-        # todo this is wrong and needs to wrap back over depending on preceding letter,
-        # i.e. bcz -> bda, bdb, bdc ...
+
+        # bcz -> bda, bdb, bdc ...
         # bzz -> caa
-        elif pw[index] == 'z':
-            index -= 1
         else:
-            char_at_index = pw[index]
-
-            i = digits_to_index[char_at_index]
-
-            new_char = index_to_digits[(i + 1) % 25]
-            print(f'current char {char_at_index} with index {i} updated to {new_char}')
-
             pw_chars = list(pw)
-            pw_chars[index] = new_char
-            pw = ''.join(pw_chars)
-            print(f'new password: {pw}')
+            char_at_index = pw[index]
+            if char_at_index == "z":
+                print(
+                    f"char at idx {index} in pw {pw} is z, setting to a and moving back"
+                )
+                pw_chars[index] = "a"
+                index -= 1
+            else:
+                curr_dig_i = digits_to_index[char_at_index]
+                next_char = index_to_digits[curr_dig_i + 1]
 
+                pw_chars[index] = next_char
 
+        pw = "".join(pw_chars)
+        index -= 1
 
 
 if __name__ == "__main__":
-    next_password = increment_password('abcdefgh')
-    print(f'next password: {next_password}')
+    next_password = increment_password("abcdefgh")
+    print(f"next password: {next_password}")
