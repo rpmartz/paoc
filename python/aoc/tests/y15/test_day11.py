023da12e9a6ecdba777aa1ff2a5a02083dd45f59
rmartz <martzrp@gmail.com>
2024-01-27 17:06:06 -0800

    tests for day11

---
+++ python/aoc/tests/y15/test_day11.py
@@ -0,0 +1,14 @@
+import unittest
+
+from python.aoc.y15.day11 import meets_requirement, increment_password
+
+class Y15Day11Test(unittest.TestCase):
+
+    def test_example_passwords(self):
+        self.assertFalse(meets_requirement('hijklmmn'))
+        self.assertFalse(meets_requirement('abbceffg'))
+        self.assertFalse(meets_requirement('abbcegjk'))
+        self.assertFalse(meets_requirement('abbcegjk'))
+        self.assertTrue(meets_requirement('abcdffaa'))
+        self.assertTrue(meets_requirement('ghjaabcc'))
+
