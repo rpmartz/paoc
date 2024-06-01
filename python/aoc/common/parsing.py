13b4c947141643bd71f52ff617a8da68abd4122c
rmartz <martzrp@gmail.com>
2024-06-01 07:40:59 -0700

    add split and parse function

--- python/aoc/common/parsing.py
+++ python/aoc/common/parsing.py
@@ -1,6 +1,11 @@
 import re
+from typing import List
 
 
 def ints(s):
     """Finds all the integers in a string"""
     return [int(v) for v in re.findall("-?[0-9]+", s)]
+
+def split_and_parse(s: str, delimiter=",", parse_fn=int):
+    xs = s.split(delimiter)
+    return [parse_fn(x) for x in xs]
