6a9ea53e9a3a87c76199ba3d433bb857dc62e777
rmartz <martzrp@gmail.com>
2025-07-31 05:59:14 -0700

    24.08: massage data structures

--- aoc/y24/day08.py
+++ aoc/y24/day08.py
@@ -1,5 +1,8 @@
 from itertools import combinations
 
+from aoc.common.geometry import manhattan_distance
+
+
 def parse_grid() -> dict:
     with open('data/day08.txt') as f:
         lines = [l.strip() for l in f.readlines()]
@@ -7,10 +10,10 @@ def parse_grid() -> dict:
     _grid = {}
     for i, line in enumerate(lines):
         for j, char in enumerate(line):
-            if char != '.':
-                antennas = _grid.get(char, set())
-                antennas.add((i, j))
-                _grid[char] = antennas
+            # filtering `.` out saves memory, but complicates downstream
+            # logic because it requires a bounds check for antinode locations,
+            # whereas including it makes bounds checking a simple `in` check
+            _grid[(i, j)] = char
 
     return _grid
 
@@ -23,12 +26,20 @@ def is_on_board(p):
 grid = parse_grid()
 antinodes = set()
 
+frequency_locations = dict()
+for coords, frequency in grid.items():
+    if frequency != '.':
+        locations = frequency_locations.get(frequency, set())
+        locations.add(coords)
+        frequency_locations[frequency] = locations
+
+
 # now we have a data structure that groups the location of each antenna by frequency
-for frequency, antenna_coordinates in grid.items():
+for frequency, antenna_coordinates in frequency_locations.items():
     # we need all of the combinations of antennas for this frequency
     _combinations = combinations(antenna_coordinates, 2)
     for a, b in _combinations:
-        print(f'{frequency} -> {a} -> {b}')
+        man_dist = abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 # print(len(antinodes))
