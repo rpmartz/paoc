7db600836ec3688e8607ba25611e4185b5499ad9
rmartz <martzrp@gmail.com>
2025-07-31 06:41:20 -0700

    24.08: part 1 answer

--- aoc/y24/day08.py
+++ aoc/y24/day08.py
@@ -1,6 +1,5 @@
 from itertools import combinations
-
-from aoc.common.geometry import manhattan_distance
+from typing import Tuple
 
 
 def parse_grid() -> dict:
@@ -17,11 +16,14 @@ def parse_grid() -> dict:
 
     return _grid
 
-def is_on_board(p):
-    i = p[0]
-    j = p[1]
+def mul_pt(pt, k) -> Tuple[int, int]:
+    """Performs scalar multiplication of a point"""
+    return pt[0] * k, pt[1] * k
 
-    return 0 <= i < 49 and 0 <= j < 49
+def sub_pt(a: Tuple[int, int], b: Tuple[int, int]) -> Tuple[int, int]:
+    """subtraction of two points"""
+    return a[0] - b[0], a[1] - b[1]
+
 
 grid = parse_grid()
 antinodes = set()
@@ -34,13 +36,22 @@ for coords, frequency in grid.items():
         frequency_locations[frequency] = locations
 
 
+
 # now we have a data structure that groups the location of each antenna by frequency
 for frequency, antenna_coordinates in frequency_locations.items():
     # we need all of the combinations of antennas for this frequency
     _combinations = combinations(antenna_coordinates, 2)
     for a, b in _combinations:
         man_dist = abs(a[0] - b[0]) + abs(a[1] - b[1])
+        # antinodes only if one is 2x distance, 2a - b and 2b - a
+        anode_1 = sub_pt(mul_pt(a, 2), b)
+        anode_2 = sub_pt(mul_pt(b, 2), a)
+
+        if anode_1 in grid:
+            antinodes.add(anode_1)
+        if anode_2 in grid:
+            antinodes.add(anode_2)
 
 
-# print(len(antinodes))
+print(len(antinodes))
 
